01534141edd6b7c153e88bcae5814b627cd414c2
mishka <mishka@macaroni.local>
2024-08-04 14:48:16 -0400

    fix

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -63,10 +63,10 @@ return {
           -- This is a current Neovim limitation.
           enabled = true,         -- enables the Noice messages UI
           view = "mini",        -- default view for messages
-          view_error = "notify",  -- view for errors
-          view_warn = "notify",   -- view for warnings
-          view_history = "messages", -- view for :messages
-          view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+          view_error = "mini",  -- view for errors
+          view_warn = "mini",   -- view for warnings
+          view_history = "mini", -- view for :messages
+          view_search = "mini", -- view for search count messages. Set to `false` to disable
         },
       })
     end,

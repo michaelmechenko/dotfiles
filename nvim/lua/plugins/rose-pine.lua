c4867635d45319a1c9365b9df8c7ebf7213554e1
mishka <mishka@macaroni.local>
2024-08-12 13:17:50 -0400

    colors

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -4,7 +4,7 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "auto",  -- auto, main, moon, or dawn
+        variant = "moon",  -- auto, main, moon, or dawn
         dark_variant = "main", -- main, moon, or dawn
         dim_inactive_windows = true,
         extend_background_behind_borders = true,
@@ -18,7 +18,7 @@ return {
         styles = {
           bold = true,
           italic = true,
-          transparency = false,
+          transparency = true,
         },
 
         groups = {

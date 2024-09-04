fec17c0f544289b3c1865fa0b9dff7b985b9a67d
michaelmechenko <michael.mechenko@gmail.com>
2024-09-04 19:43:02 -0400

    fixes

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,18 +1,18 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
+  "lukas-reineke/indent-blankline.nvim",
   event = "VeryLazy",
-	main = "ibl",
-	opts = {},
-	config = function()
-		require("ibl").setup({
-			-- indent = {
-			-- 	char = "┊",
-			--      -- highlight = "IblIndent"
-			-- },
-			scope = {
-				char = "▏",
-				-- highlight = "IblScope"
-			},
-		})
-	end,
+  main = "ibl",
+  opts = {},
+  config = function()
+    require("ibl").setup({
+      -- indent = {
+      -- 	char = "┊",
+      --      -- highlight = "IblIndent"
+      -- },
+      scope = {
+        	char = "▏",
+        -- highlight = "IblScope"
+      },
+    })
+  end,
 }
--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,217 +1,220 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		local wk = require("which-key")
-		wk.add({
-			-- avante
-			{ "<leader>a", group = "avante" },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    local wk = require("which-key")
+    wk.add({
+      -- avante
+      { "<leader>a",  group = "avante" },
 
-			-- chad gpt
-			{ "<leader>q", group = "ai" },
-			{ "<leader>qq", "<cmd>ChatGPT<CR>", desc = "chatgpt" },
-			{ "<leader>qe", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "edit w instruction", mode = { "n", "v" } },
-			{
-				"<leader>qg",
-				"<cmd>ChatGPTRun grammar_correction<CR>",
-				desc = "grammar Correction",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qt",
-				"<cmd>ChatGPTRun translate<CR>",
-				desc = "translate",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qk",
-				"<cmd>ChatGPTRun keywords<CR>",
-				desc = "keywords",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qd",
-				"<cmd>ChatGPTRun docstring<CR>",
-				desc = "docstring",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qa",
-				"<cmd>ChatGPTRun add_tests<CR>",
-				desc = "add tests",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qo",
-				"<cmd>ChatGPTRun optimize_code<CR>",
-				desc = "optimize code",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qs",
-				"<cmd>ChatGPTRun summarize<CR>",
-				desc = "summarize",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qf",
-				"<cmd>ChatGPTRun fix_bugs<CR>",
-				desc = "fix bugs",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qx",
-				"<cmd>ChatGPTRun explain_code<CR>",
-				desc = "explain code",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>qr",
-				"<cmd>ChatGPTRun roxygen_edit<CR>",
-				desc = "roxygen edit",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>ql",
-				"<cmd>ChatGPTRun code_readability_analysis<CR>",
-				desc = "code readibility analytics",
-				mode = { "n", "v" },
-			},
+      -- chad gpt
+      { "<leader>q",  group = "ai" },
+      { "<leader>qq", "<cmd>ChatGPT<CR>",                    desc = "chatgpt" },
+      { "<leader>qe", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "edit w instruction", mode = { "n", "v" } },
+      {
+        "<leader>qg",
+        "<cmd>ChatGPTRun grammar_correction<CR>",
+        desc = "grammar Correction",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qt",
+        "<cmd>ChatGPTRun translate<CR>",
+        desc = "translate",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qk",
+        "<cmd>ChatGPTRun keywords<CR>",
+        desc = "keywords",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qd",
+        "<cmd>ChatGPTRun docstring<CR>",
+        desc = "docstring",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qa",
+        "<cmd>ChatGPTRun add_tests<CR>",
+        desc = "add tests",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qo",
+        "<cmd>ChatGPTRun optimize_code<CR>",
+        desc = "optimize code",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qs",
+        "<cmd>ChatGPTRun summarize<CR>",
+        desc = "summarize",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qf",
+        "<cmd>ChatGPTRun fix_bugs<CR>",
+        desc = "fix bugs",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qx",
+        "<cmd>ChatGPTRun explain_code<CR>",
+        desc = "explain code",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>qr",
+        "<cmd>ChatGPTRun roxygen_edit<CR>",
+        desc = "roxygen edit",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ql",
+        "<cmd>ChatGPTRun code_readability_analysis<CR>",
+        desc = "code readibility analytics",
+        mode = { "n", "v" },
+      },
 
-			-- neotree
-			{ "<leader>r", group = "noeotree" },
-			{ "<leader>rr", "<cmd>Neotree filesystem reveal right<CR>", desc = "filesystem" },
-			{ "<leader>rw", "<cmd>Neotree buffers reveal right<CR>", desc = "buffers" },
-			{ "<leader>rq", "<cmd>Neotree close<CR>", desc = "close" },
-			{ "<leader>rb", "<cmd>Neotree git_status reveal float<CR>", desc = "git status" },
+      -- neotree
+      { "<leader>r",  group = "noeotree" },
+      { "<leader>rr", "<cmd>Neotree filesystem reveal right<CR>", desc = "filesystem" },
+      { "<leader>rw", "<cmd>Neotree buffers reveal right<CR>",    desc = "buffers" },
+      { "<leader>rq", "<cmd>Neotree close<CR>",                   desc = "close" },
+      { "<leader>rb", "<cmd>Neotree git_status reveal float<CR>", desc = "git status" },
 
-			-- trouble
-			{ "<leader>x", group = "trouble" },
-			{
-				"<leader>xx",
-				function()
-					if require("trouble").is_open() then
-						require("trouble").focus({ true })
-					end
-				end,
-				desc = "focus",
-			},
-			{
-				"<leader>xr",
-				function()
-					require("trouble").refresh({ true })
-				end,
-				desc = "refresh",
-			},
-			{
-				"<leader>xq",
-				function()
-					if require("trouble").is_open() then
-						require("trouble").close({})
-					end
-				end,
-				desc = "close",
-			},
-			{
-				"<leader>xd",
-				function()
-					require("trouble").toggle("diagnostics")
-				end,
-				desc = "diagnostics",
-			},
-			{
-				"<leader>xs",
-				"<cmd>Trouble toggle symbols win.size=0.3<CR>",
-				-- function()
-				--   require("trouble").toggle("symbols")
-				-- end,
-				desc = "symbols",
-			},
-			{
-				"<leader>xl",
-				"<cmd>Trouble toggle lsp win.size=0.3<CR>",
-				-- function()
-				--   require("trouble").toggle("lsp")
-				-- end,
-				desc = "lsp",
-			},
+      -- trouble
+      { "<leader>x",  group = "trouble" },
+      {
+        "<leader>xx",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").focus({ true })
+          end
+        end,
+        desc = "focus",
+      },
+      {
+        "<leader>xr",
+        function()
+          require("trouble").refresh({ true })
+        end,
+        desc = "refresh",
+      },
+      {
+        "<leader>xq",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").close({})
+          end
+        end,
+        desc = "close",
+      },
+      {
+        "<leader>xd",
+        function()
+          require("trouble").toggle("diagnostics")
+        end,
+        desc = "diagnostics",
+      },
+      {
+        "<leader>xs",
+        "<cmd>Trouble toggle symbols win.size=0.3<CR>",
+        -- function()
+        --   require("trouble").toggle("symbols")
+        -- end,
+        desc = "symbols",
+      },
+      {
+        "<leader>xl",
+        "<cmd>Trouble toggle lsp win.size=0.3<CR>",
+        -- function()
+        --   require("trouble").toggle("lsp")
+        -- end,
+        desc = "lsp",
+      },
 
-			-- lsp
-			{ "<leader>l", group = "lsp" },
-			{ "<leader>ll", vim.lsp.buf.hover, desc = "hover" },
-			{ "<leader>lo", vim.diagnostic.open_float, desc = "open diagnostic float" },
-			{ "<leader>lk", vim.lsp.buf.definition, desc = "definition" },
-			{ "<leader>lm", vim.lsp.buf.code_action, desc = "code action" },
+      -- lsp
+      { "<leader>l",  group = "lsp" },
+      { "<leader>ll", vim.lsp.buf.hover,         desc = "hover" },
+      { "<leader>lo", vim.diagnostic.open_float, desc = "open diagnostic float" },
+      { "<leader>lk", vim.lsp.buf.definition,    desc = "definition" },
+      { "<leader>lm", vim.lsp.buf.code_action,   desc = "code action" },
 
-			-- telescope
-			{ "<leader>f", group = "telescope" },
-			{
-				"<leader>ff",
-				function()
-					require("telescope.builtin").find_files()
-				end,
-				desc = "find files",
-			},
-			{
-				"<leader>fd",
-				function()
-					require("telescope.builtin").live_grep()
-				end,
-				desc = "live grep",
-			},
-			{
-				"<leader>fm",
-				function()
-					require("telescope.builtin").man_pages()
-				end,
-				desc = "man pages",
-			},
-			{
-				"<leader>fq",
-				function()
-					require("telescope.builtin").quickfix()
-				end,
-				desc = "quickfix",
-			},
-			{
-				"<leader>ft",
-				function()
-					require("telescope.builtin").treesitter()
-				end,
-				desc = "treesitter",
-			},
+      -- telescope
+      { "<leader>f",  group = "telescope" },
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "find files",
+      },
+      {
+        "<leader>fd",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "live grep",
+      },
+      {
+        "<leader>fm",
+        function()
+          require("telescope.builtin").man_pages()
+        end,
+        desc = "man pages",
+      },
+      {
+        "<leader>fq",
+        function()
+          require("telescope.builtin").quickfix()
+        end,
+        desc = "quickfix",
+      },
+      {
+        "<leader>ft",
+        function()
+          require("telescope.builtin").treesitter()
+        end,
+        desc = "treesitter",
+      },
 
-			-- hover
-			{ "<leader>h", group = "hover" },
-			{
-				"<leader>hh",
-				function()
-					require("hover").hover()
-				end,
-				desc = "hover",
-			},
-			{
-				"<leader>hs",
-				function()
-					require("hover").hover_select()
-				end,
-				desc = "hover select",
-			},
-			{
-				"<leader>hq",
-				function()
-					require("hover").hover_switch("previous")
-				end,
-				desc = "previous source",
-			},
-			{
-				"<leader>he",
-				function()
-					require("hover").hover_switch("next")
-				end,
-				desc = "next source source",
-			},
-		})
-	end,
-	opts = {},
+      -- hover
+      { "<leader>h", group = "hover" },
+      {
+        "<leader>hh",
+        function()
+          require("hover").hover()
+        end,
+        desc = "hover",
+      },
+      {
+        "<leader>hs",
+        function()
+          require("hover").hover_select()
+        end,
+        desc = "hover select",
+      },
+      {
+        "<leader>hq",
+        function()
+          require("hover").hover_switch("previous")
+        end,
+        desc = "previous source",
+      },
+      {
+        "<leader>he",
+        function()
+          require("hover").hover_switch("next")
+        end,
+        desc = "next source source",
+      },
+
+      -- surround
+      { "<leader>s", group = "surround" },
+    })
+  end,
+  opts = {},
 }

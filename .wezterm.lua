ae1f7ddf1afca68982f44940536896eaca755090
michaelmechenko <michael.mechenko@gmail.com>
2024-09-10 17:04:40 -0400

    newtabs are #s

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,27 +1,31 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 local act = wezterm.action
+local mux = wezterm.mux
 
-local wezterm = require("wezterm")
 local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 
-function num_tabs(window, pane)
+local function num_tabs(window)
 	local tab_arr = window:mux_window():tabs()
-
 	return #tab_arr
 end
 
-function tabs_greater_than_1(window, pane)
-	return num_tabs(window, pane) > 1
+local function tabs_greater_than_1(window)
+	return num_tabs(window) > 1
 end
 
-local default_top_padding = "0.3cell"
+wezterm.on("gui-startup", function(cmd)
+	local tab = mux.spawn_window(cmd or {})
+	local new_title = "1"
+	tab:set_title(new_title)
+end)
+
+local default_top_padding = "0.4cell"
 local default_bottom_padding = "0.0cell"
 local default_left_padding = "0.6cell"
 local default_right_padding = "0.6cell"
 
-local tabs_top_padding = "0.1cell"
+local tabs_top_padding = "0.0cell"
 local tabs_bottom_padding = "0.0cell"
 local tabs_left_padding = "0.6cell"
 local tabs_right_padding = "0.6cell"
@@ -29,6 +33,9 @@ local tabs_right_padding = "0.6cell"
 -- general terminal settings
 config.window_close_confirmation = "AlwaysPrompt"
 config.color_scheme = "rose-pine"
+config.show_tab_index_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+-- config.tab_bar_at_bottom = true
 -- config.use_fancy_tab_bar = false
 
 -- remove if not on nightly
@@ -36,7 +43,6 @@ config.show_close_tab_button_in_tabs = false
 
 -- tab bar
 config.window_decorations = "RESIZE"
-config.tab_max_width = 24
 config.enable_tab_bar = false
 -- config.hide_tab_bar_if_only_one_tab = true
 
@@ -111,9 +117,9 @@ config.keys = {
 		key = "w",
 		mods = "CMD|SHIFT",
 		action = act.Multiple({
-			wezterm.action_callback(function(win, pane)
-				resurrect.save_state(resurrect.workspace_state.get_workspace_state())
-			end),
+			-- wezterm.action_callback(function(win, pane)
+			-- resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+			-- end),
 			act.CloseCurrentTab({ confirm = true }),
 		}),
 	},
@@ -121,82 +127,96 @@ config.keys = {
 	{
 		key = "s",
 		mods = "CMD|SHIFT",
-		action = wezterm.action_callback(function(win, pane)
+		action = wezterm.action_callback(function()
 			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
-      resurrect.window_state.save_window_action()
+			resurrect.window_state.save_window_action()
 		end),
 	},
 
 	{
 		key = "x",
 		mods = "CMD|SHIFT",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		action = act.CloseCurrentPane({ confirm = true }),
 	},
 
 	{
 		key = "RightArrow",
 		mods = "CMD|CTRL",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 
 	{
 		key = "DownArrow",
 		mods = "CMD|CTRL",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 
 	{
 		key = "LeftArrow",
 		mods = "CMD|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Left"),
+		action = act.ActivatePaneDirection("Left"),
 	},
 
 	{
 		key = "RightArrow",
 		mods = "CMD|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Right"),
+		action = act.ActivatePaneDirection("Right"),
 	},
 
 	{
 		key = "UpArrow",
 		mods = "CMD|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Up"),
+		action = act.ActivatePaneDirection("Up"),
 	},
 
 	{
 		key = "DownArrow",
 		mods = "CMD|SHIFT",
-		action = wezterm.action.ActivatePaneDirection("Down"),
+		action = act.ActivatePaneDirection("Down"),
 	},
+
 	{
 		key = "LeftArrow",
 		mods = "CMD|SHIFT|ALT",
-		action = wezterm.action.AdjustPaneSize({ "Left", 3 }),
+		action = act.AdjustPaneSize({ "Left", 3 }),
 	},
 
 	{
 		key = "RightArrow",
 		mods = "CMD|SHIFT|ALT",
-		action = wezterm.action.AdjustPaneSize({ "Right", 3 }),
+		action = act.AdjustPaneSize({ "Right", 3 }),
 	},
 
 	{
 		key = "UpArrow",
 		mods = "CMD|SHIFT|ALT",
-		action = wezterm.action.AdjustPaneSize({ "Up", 3 }),
+		action = act.AdjustPaneSize({ "Up", 3 }),
 	},
 
 	{
 		key = "DownArrow",
 		mods = "CMD|SHIFT|ALT",
-		action = wezterm.action.AdjustPaneSize({ "Down", 3 }),
+		action = act.AdjustPaneSize({ "Down", 3 }),
+	},
+
+	{
+		key = "t",
+		mods = "CMD",
+		action = act.Multiple({
+			act.SpawnTab("CurrentPaneDomain"),
+			wezterm.action_callback(function(window)
+				local tab_num = num_tabs(window)
+				local new_title = tab_num
+				window:active_tab():set_title(new_title)
+			end),
+		}),
 	},
 
 	{
 		key = "O",
 		mods = "CMD|SHIFT",
 		action = wezterm.action_callback(function(win, pane)
-			resurrect.fuzzy_load(win, pane, function(id, label)
+			resurrect.fuzzy_load(win, pane, function(id)
 				local type = string.match(id, "^([^/]+)") -- match before '/'
 				id = string.match(id, "([^/]+)$") -- match after '/'
 				id = string.match(id, "(.+)%..+$") -- remove file extension
@@ -227,7 +247,7 @@ config.keys = {
 		mods = "CMD|SHIFT",
 		action = act.PromptInputLine({
 			description = "Enter new name for tab",
-			action = wezterm.action_callback(function(window, pane, line)
+			action = wezterm.action_callback(function(window, line)
 				-- line will be `nil` if they hit escape without entering anything
 				-- An empty string if they just hit enter
 				-- Or the actual line of text they wrote
@@ -239,9 +259,9 @@ config.keys = {
 	},
 }
 
-wezterm.on("update-right-status", function(window, pane)
+wezterm.on("update-right-status", function(window)
 	local overrides = window:get_config_overrides() or {}
-	local tabs_gt_1 = tabs_greater_than_1(window, pane)
+	local tabs_gt_1 = tabs_greater_than_1(window)
 
 	overrides.enable_tab_bar = tabs_gt_1
 	if tabs_gt_1 then
@@ -249,14 +269,14 @@ wezterm.on("update-right-status", function(window, pane)
 			top = tabs_top_padding,
 			bottom = tabs_bottom_padding,
 			left = tabs_left_padding,
-			right = tabs_bottom_padding,
+			right = tabs_right_padding,
 		}
 	else
 		overrides.window_padding = {
 			top = default_top_padding,
 			bottom = default_bottom_padding,
 			left = default_left_padding,
-			right = default_bottom_padding,
+			right = default_right_padding,
 		}
 	end
 

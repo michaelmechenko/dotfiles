d8e9cfde5b3814e0f3008ca11d37b49e8281367d
michaelmechenko <michael.mechenko@gmail.com>
2025-01-23 14:29:29 -0500

    yank global reg

--- nvim/lua/vim-keymaps.lua
+++ nvim/lua/vim-keymaps.lua
@@ -14,6 +14,12 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+-- yanking/pasting to global register
+vim.keymap.set("n", "P", "\"+p", {})
+vim.keymap.set("n", "Y", "\"+y", {})
+vim.keymap.set("v", "P", "\"+p", {})
+vim.keymap.set("v", "Y", "\"+y", {})
+
 -- buffer remaps
 vim.keymap.set("n", "<leader>bd", ":bd<CR>", {})
 vim.keymap.set("n", "<leader>bc", ":bprev<CR>", {})

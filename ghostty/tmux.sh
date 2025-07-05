81167457142c1cd79724d4e3f5feb3497d936e8e
michaelmechenko <michael.mechenko@gmail.com>
2025-07-04 22:05:25 -0400

    dont immediately start new tmux session

--- ghostty/tmux.sh
+++ ghostty/tmux.sh
@@ -8,8 +8,8 @@ SESSION_NAME="ghostty"
 if [ $? -eq 0 ]; then
   # If the session exists, reattach to it
   /opt/homebrew/bin/tmux attach-session -t $SESSION_NAME
-else
+# else
   # If the session doesn't exist, start a new one
-  /opt/homebrew/bin/tmux new-session -s $SESSION_NAME -d
-  /opt/homebrew/bin/tmux attach-session -t $SESSION_NAME
+  # /opt/homebrew/bin/tmux new-session -s $SESSION_NAME -d
+  # /opt/homebrew/bin/tmux attach-session -t $SESSION_NAME
 fi

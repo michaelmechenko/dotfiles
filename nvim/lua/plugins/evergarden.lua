42dfebf6928914ae97cc5e9267cb8069f8b0ef69
michaelmechenko <michael.mechenko@gmail.com>
2025-08-13 20:15:41 -0400

    clean unused plugins

--- nvim/lua/plugins/evergarden.lua
+++ nvim/lua/plugins/evergarden.lua
@@ -1,33 +1,33 @@
 return {
-  "everviolet/nvim",
-  name = "evergarden",
-  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = "winter", -- 'winter'|'fall'|'spring'|'summer'
-      accent = "pink",
-    },
-    editor = {
-      transparent_background = true,
-      override_terminal = true,
-      sign = { color = "none" },
-      float = {
-        color = "mantle",
-        invert_border = false,
-      },
-      completion = {
-        color = "surface0",
-      },
-    },
-    style = {
-      tabline = { "reverse" },
-      search = { "italic", "reverse" },
-      incsearch = { "italic", "reverse" },
-      types = { "italic" },
-      keyword = { "italic" },
-      comment = { "italic" },
-    },
-    overrides = {},
-    color_overrides = {},
-  },
+  -- "everviolet/nvim",
+  -- name = "evergarden",
+  -- priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+  -- opts = {
+  --   theme = {
+  --     variant = "winter", -- 'winter'|'fall'|'spring'|'summer'
+  --     accent = "pink",
+  --   },
+  --   editor = {
+  --     transparent_background = true,
+  --     override_terminal = true,
+  --     sign = { color = "none" },
+  --     float = {
+  --       color = "mantle",
+  --       invert_border = false,
+  --     },
+  --     completion = {
+  --       color = "surface0",
+  --     },
+  --   },
+  --   style = {
+  --     tabline = { "reverse" },
+  --     search = { "italic", "reverse" },
+  --     incsearch = { "italic", "reverse" },
+  --     types = { "italic" },
+  --     keyword = { "italic" },
+  --     comment = { "italic" },
+  --   },
+  --   overrides = {},
+  --   color_overrides = {},
+  -- },
 }
--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -1,97 +1,97 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      require("kanagawa").setup({
-        compile = false,
-        undercurl = true,
-        commentStyle = { italic = true },
-        functionStyle = { bold = true },
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true,
-        dimInactive = true,
-        terminalColors = true,
-        colors = {
-          palette = {
-            -- normal
-            -- 938AA9
-            -- 957FB8
-            -- 8f80b2
-            crystalBlue = "#8F80B2",
-            -- insert
-            -- springGreen = "#76946A",
-            -- visual
-            -- 8092b2
-            -- 9cabca
-            oniViolet = "#8092B2",
-            -- replace
-            -- surimiOrange = "#cf8c8e",
-            -- comments
-            -- fujiGray = "#63637b",
-          },
-          theme = {
-            wave = {},
-            lotus = {},
-            dragon = {},
-            all = {
-              ui = {
-                bg_gutter = "none",
-              },
-            },
-          },
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-
-          return {
-            LineNr = { fg = "#57576b" },
-            LineNrAbove = { fg = "#57576b" },
-            LineNrBelow = { fg = "#57576b" },
-            CursorLineNr = { fg = "#938AA9", bold = true },
-            -- CurSearch = { bg = "#252525" },
-            -- CursorColumn = { bg = "#252525" },
-            -- CursorLine = { bg = "#252525" },
-            CursorLine = { bg = "#2a2e40" },
-            -- CursorLineNr = { bold = true },
-            -- NonText = { fg = theme.ui.bg_p1, bg = "none" },
-
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-            MiniFilesTitleFocused = { fg = "#938AA9", bg = "none" },
-            MiniFilesTitle = { fg = "#57576b", bg = "none" },
-            -- MiniFilesTitleFocused = { fg = "#938AA9", bg = theme.ui.bg_p1 },
-            -- MiniFilesTitle = { fg = "#57576b", bg = theme.ui.bg_p1 },
-
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-            Visual = { bg = "#54546D" },
-          }
-        end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          -- dark = "dragon", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-
-      -- vim.cmd("colorscheme kanagawa")
-    end,
-  },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   config = function()
+  --     require("kanagawa").setup({
+  --       compile = false,
+  --       undercurl = true,
+  --       commentStyle = { italic = true },
+  --       functionStyle = { bold = true },
+  --       keywordStyle = { italic = true },
+  --       statementStyle = { bold = true },
+  --       typeStyle = {},
+  --       transparent = true,
+  --       dimInactive = true,
+  --       terminalColors = true,
+  --       colors = {
+  --         palette = {
+  --           -- normal
+  --           -- 938AA9
+  --           -- 957FB8
+  --           -- 8f80b2
+  --           crystalBlue = "#8F80B2",
+  --           -- insert
+  --           -- springGreen = "#76946A",
+  --           -- visual
+  --           -- 8092b2
+  --           -- 9cabca
+  --           oniViolet = "#8092B2",
+  --           -- replace
+  --           -- surimiOrange = "#cf8c8e",
+  --           -- comments
+  --           -- fujiGray = "#63637b",
+  --         },
+  --         theme = {
+  --           wave = {},
+  --           lotus = {},
+  --           dragon = {},
+  --           all = {
+  --             ui = {
+  --               bg_gutter = "none",
+  --             },
+  --           },
+  --         },
+  --       },
+  --       overrides = function(colors)
+  --         local theme = colors.theme
+  --
+  --         return {
+  --           LineNr = { fg = "#57576b" },
+  --           LineNrAbove = { fg = "#57576b" },
+  --           LineNrBelow = { fg = "#57576b" },
+  --           CursorLineNr = { fg = "#938AA9", bold = true },
+  --           -- CurSearch = { bg = "#252525" },
+  --           -- CursorColumn = { bg = "#252525" },
+  --           -- CursorLine = { bg = "#252525" },
+  --           CursorLine = { bg = "#2a2e40" },
+  --           -- CursorLineNr = { bold = true },
+  --           -- NonText = { fg = theme.ui.bg_p1, bg = "none" },
+  --
+  --           NormalFloat = { bg = "none" },
+  --           FloatBorder = { bg = "none" },
+  --           FloatTitle = { bg = "none" },
+  --
+  --           -- Save an hlgroup with dark background and dimmed foreground
+  --           -- so that you can use it where your still want darker windows.
+  --           -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+  --           NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+  --
+  --           -- Popular plugins that open floats will link to NormalFloat by default;
+  --           -- set their background accordingly if you wish to keep them dark and borderless
+  --           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+  --           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+  --
+  --           MiniFilesTitleFocused = { fg = "#938AA9", bg = "none" },
+  --           MiniFilesTitle = { fg = "#57576b", bg = "none" },
+  --           -- MiniFilesTitleFocused = { fg = "#938AA9", bg = theme.ui.bg_p1 },
+  --           -- MiniFilesTitle = { fg = "#57576b", bg = theme.ui.bg_p1 },
+  --
+  --           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+  --           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+  --           PmenuSbar = { bg = theme.ui.bg_m1 },
+  --           PmenuThumb = { bg = theme.ui.bg_p2 },
+  --           Visual = { bg = "#54546D" },
+  --         }
+  --       end,
+  --       theme = "wave", -- Load "wave" theme when 'background' option is not set
+  --       background = { -- map the value of 'background' option to a theme
+  --         dark = "wave", -- try "dragon" !
+  --         -- dark = "dragon", -- try "dragon" !
+  --         light = "lotus",
+  --       },
+  --     })
+  --
+  --     -- vim.cmd("colorscheme kanagawa")
+  --   end,
+  -- },
 }

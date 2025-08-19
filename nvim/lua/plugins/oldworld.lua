cdaf2808d363fe0a263a514b81f71d19163d0587
michaelmechenko <michael.mechenko@gmail.com>
2025-08-19 02:51:11 -0400

    no change

--- nvim/lua/plugins/oldworld.lua
+++ nvim/lua/plugins/oldworld.lua
@@ -5,17 +5,17 @@ return {
     priority = 1000,
     config = function()
       require("oldworld").setup({
-        terminal_colors = true,    -- enable terminal colors
-        variant = "cooler",       -- default, oled, cooler
-        styles = {                 -- You can pass the style using the format: style = true
+        terminal_colors = true,         -- enable terminal colors
+        variant = "cooler",             -- default, oled, cooler
+        styles = {                      -- You can pass the style using the format: style = true
           comments = { italic = true }, -- style for comments
-          keywords = { bold = true }, -- style for keywords
-          identifiers = {},        -- style for identifiers
-          functions = { bold = true }, -- style for functions
+          keywords = { bold = true },   -- style for keywords
+          identifiers = {},             -- style for identifiers
+          functions = { bold = true },  -- style for functions
           variables = { bold = false }, -- style for variables
-          booleans = {},           -- style for booleans
+          booleans = {},                -- style for booleans
         },
-        integrations = {           -- You can disable/enable integrations
+        integrations = {                -- You can disable/enable integrations
           alpha = true,
           cmp = true,
           flash = true,
@@ -38,7 +38,7 @@ return {
         },
         highlight_overrides = {
           CursorLineNr = { bold = true },
-          Variable = { fg = "#938AA9" }
+          Variable = { fg = "#938AA9" },
         },
       })
       vim.cmd("colorscheme oldworld")

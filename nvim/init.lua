bda3c4bab9cc7c87c21b2de91f1c9683858e882a
michaelmechenko <michael.mechenko@gmail.com>
2025-10-01 03:27:38 -0400

    add autoread command

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,12 @@ vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.conceallevel = 0
 
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,

4422be0b7d03f13db34afe59f56b6524d5a6391d
michaelmechenko <michael.mechenko@gmail.com>
2025-10-02 20:30:53 -0400

    remove hover on mouse enable

--- nvim/lua/plugins/hover.lua
+++ nvim/lua/plugins/hover.lua
@@ -8,7 +8,7 @@ return {
         -- require('hover.providers.gh')
         -- require('hover.providers.gh_user')
         -- require('hover.providers.jira')
-        require('hover.providers.dap')
+        -- require('hover.providers.dap')
         -- require('hover.providers.fold_preview')
         require("hover.providers.diagnostic")
         require("hover.providers.man")
@@ -19,9 +19,8 @@ return {
       -- Whether the contents of a currently open hover window should be moved
       -- to a :h preview-window when pressing the hover keymap.
       preview_window = false,
-      title = true,
+      title = false,
       mouse_providers = {
-        -- "LSP",
       },
       mouse_delay = 1000,
     })
@@ -32,9 +31,5 @@ return {
     vim.keymap.set('n', 'gK', function()
       require('hover').enter()
     end, { desc = 'hover.nvim (enter)' })
-
-    -- Mouse support
-    vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
-    vim.o.mousemoveevent = true
   end,
 }

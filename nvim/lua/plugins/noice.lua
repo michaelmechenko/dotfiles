ad2c7682699851e92d8e4a8cc3ca23f3f4577f5e
michaelmechenko <michael.mechenko@gmail.com>
2025-10-05 23:57:04 -0400

    change noice rendering

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,74 +1,87 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			-- "rcarriga/nvim-notify",
-		},
-
-		config = function()
-			local noice = require("noice")
-			noice.setup({
-				cmdline = {
-					enabled = true, -- enables the Noice cmdline UI
-					view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-					opts = {}, -- global options for the cmdline. See section on views
-					---@type table<string, CmdlineFormat>
-					format = {
-						-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-						-- view: (default is cmdline view)
-						-- opts: any options passed to the view
-						-- icon_hl_group: optional hl_group for the icon
-						-- title: set to anything or empty string to hide
-						cmdline = { pattern = "^:", icon = "»", lang = "vim" },
-						search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-						search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-						filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-						lua = {
-							pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" },
-							icon = "",
-							lang = "lua",
-						},
-						help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
-						input = {}, -- Used by input()
-						-- lua = false, -- to disable a format, set to `false`
-					},
-				},
-				lsp = {
-					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-					},
-				},
-				-- you can enable a preset for easier configuration
-				presets = {
-					bottom_search = true, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = false, -- add a border to hover docs and signature help
-				},
-				messages = {
-					-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-					-- This is a current Neovim limitation.
-					enabled = true, -- enables the Noice messages UI
-					view = "mini", -- default view for messages
-					view_error = "mini", -- view for errors
-					view_warn = "mini", -- view for warnings
-					view_history = "mini", -- view for :messages
-					view_search = "mini", -- view for search count messages. Set to `false` to disable
-				},
-			})
-		end,
-	},
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      -- `nvim-notify` is only needed, if you want to use the notification view.
+      -- "rcarriga/nvim-notify",
+    },
+    config = function()
+      local noice = require("noice")
+      noice.setup({
+        cmdline = {
+          enabled = true,
+          view = "cmdline",
+          opts = {},
+          format = {
+            cmdline = { pattern = "^:", icon = "»", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+            search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+            filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+            lua = {
+              pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" },
+              icon = "",
+              lang = "lua",
+            },
+            help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
+            input = {}, -- Used by input()
+            -- lua = false, -- to disable a format, set to `false`
+          },
+        },
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+          },
+          hover = {
+            enabled = true,
+            silent = true, -- set to true to not show a message if hover is not available
+            view = nil,    -- when nil, use defaults from documentation
+            ---@type NoiceViewOptions
+            opts = {},     -- merged with defaults from documentation
+          },
+          signature = {
+            enabled = false,
+            auto_open = {
+              enabled = false,
+              trigger = true,
+              luasnip = true,
+              throttle = 50,
+            },
+            view = "cmdline_popup", -- when nil, use defaults from documentation
+            opts = {},              -- merged with defaults from documentation
+          },
+          message = {
+            enabled = true,
+            view = "mini",
+            opts = {},
+          },
+          documentation = {
+            view = "hover",
+            opts = {
+              lang = "markdown",
+              replace = false,
+              render = "plain",
+              format = { "{message}" },
+              win_options = { concealcursor = "n", conceallevel = 3 },
+            },
+          },
+        },
+        presets = {
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+        messages = {
+          enabled = true,        -- enables the Noice messages UI
+          view = "mini",         -- default view for messages
+          view_error = "mini",   -- view for errors
+          view_warn = "mini",    -- view for warnings
+          view_history = "mini", -- view for :messages
+          view_search = "mini",  -- view for search count messages. Set to `false` to disable
+        },
+      })
+    end,
+  },
 }

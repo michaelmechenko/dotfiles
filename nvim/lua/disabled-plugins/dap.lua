df2b9863c226e968116215d7a91aa5a190b06b28
michaelmechenko <michael.mechenko@gmail.com>
2025-10-05 23:56:26 -0400

    move disabled plugins

---
+++ nvim/lua/disabled-plugins/dap.lua
@@ -0,0 +1,44 @@
+return {
+  { "mfussenegger/nvim-dap" },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    event = "VeryLazy",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        -- "clangd",
+        -- "codelldb",
+      },
+    },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-treesitter" },
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+  },
+}

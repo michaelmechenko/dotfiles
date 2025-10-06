ad49c4762052a2c0e3c9ecc0801f6a2ffdc1a7e8
michaelmechenko <michael.mechenko@gmail.com>
2025-10-05 23:57:00 -0400

    update hover config

--- nvim/lua/plugins/hover.lua
+++ nvim/lua/plugins/hover.lua
@@ -1,35 +1,34 @@
 return {
   "lewis6991/hover.nvim",
-  config = function()
-    require("hover").setup({
-      init = function()
-        -- Require providers
-        require("hover.providers.lsp")
-        -- require('hover.providers.gh')
-        -- require('hover.providers.gh_user')
-        -- require('hover.providers.jira')
-        -- require('hover.providers.dap')
-        -- require('hover.providers.fold_preview')
-        require("hover.providers.diagnostic")
-        require("hover.providers.man")
-      end,
+  opts = function()
+    require('hover').config({
+      providers = {
+        'hover.providers.diagnostic',
+        'hover.providers.lsp',
+        'hover.providers.dap',
+        'hover.providers.man',
+        'hover.providers.fold_preview',
+        -- 'hover.providers.highlight',
+        -- 'hover.providers.dictionary',
+        'hover.providers.gh',
+        -- 'hover.providers.gh_user',
+        -- 'hover.providers.jira',
+      },
       preview_opts = {
         border = "single",
       },
       -- Whether the contents of a currently open hover window should be moved
       -- to a :h preview-window when pressing the hover keymap.
       preview_window = false,
-      title = false,
-      mouse_providers = {
-      },
-      mouse_delay = 1000,
+      title = true,
     })
+  end,
+  config = function()
+    vim.keymap.set("n", "K", require("hover").hover, { desc = "hover" })
 
-    -- Setup keymaps
-    vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
-
-    vim.keymap.set('n', 'gK', function()
-      require('hover').enter()
-    end, { desc = 'hover.nvim (enter)' })
+    vim.keymap.set('n', 'gK',
+      function()
+        require('hover').enter()
+      end, { desc = 'hover (enter)' })
   end,
 }

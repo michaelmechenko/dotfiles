eb657d7a892d55a383af21ab3a5571ad7eff2d8c
michaelmechenko <michael.mechenko@gmail.com>
2025-10-06 01:59:28 -0400

    update md config

--- nvim/lua/plugins/render-md.lua
+++ nvim/lua/plugins/render-md.lua
@@ -1,24 +1,68 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-		ft = { "markdown", "codecompanion" },
-		config = function()
-			require("render-markdown").setup({
-				heading = { sign = true, icons = { "- ", "- ", "- ", "- ", "- ", "- " }, position = "inline" },
-				latex = {
-					enabled = true,
-					converter = "latex2text",
-					highlight = "RenderMarkdownMath",
-					top_pad = 0,
-					bottom_pad = 0,
-				},
-			})
-		end,
-	},
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    ft = { "markdown", "codecompanion" },
+    config = function()
+      require("render-markdown").setup({
+        heading = {
+          sign = true,
+          icons = { "- ", "- ", "- ", "- ", "- ", "- " },
+          position = "inline",
+          width = "block",
+          min_width = 35
+        },
+        indent = {
+          enabled = false
+        },
+        code = {
+          enabled = true,
+          -- width = 'block',
+          -- left_pad = 0.2,
+          -- right_pad = 0.2,
+          sign = false,
+        },
+        quote = {
+          enabled = false,
+        },
+        patterns = {
+          -- Highlight patterns to disable for filetypes, i.e. lines concealed around code blocks
+          markdown = {
+            disable = true,
+            directives = {
+              { id = 17, name = 'conceal_lines' },
+              { id = 18, name = 'conceal_lines' },
+            },
+          },
+        },
+        anti_conceal = {
+          enabled = true,
+        },
+        bullet = {
+          enabled = false,
+        },
+        checkbox = {
+          enabled = false,
+        },
+        link = {
+          enabled = false,
+          footnote = {
+            enabled = false,
+          },
+        },
+        latex = {
+          enabled = false,
+          converter = "latex2text",
+          highlight = "RenderMarkdownMath",
+          top_pad = 0,
+          bottom_pad = 0,
+        },
+      })
+    end,
+  },
 }

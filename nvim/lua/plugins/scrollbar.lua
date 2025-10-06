17e0ceb53daac2573450596349d6a73714c30dd3
michaelmechenko <michael.mechenko@gmail.com>
2025-10-05 23:57:09 -0400

    change scrollbar color

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -1,162 +1,163 @@
 return {
-	{
-		"petertriho/nvim-scrollbar",
-		config = function()
-			require("scrollbar").setup({
-				show = true,
-				show_in_active_only = true,
-				set_highlights = true,
-				folds = true, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
-				max_lines = false, -- disables if no. of lines in buffer exceeds this
-				hide_if_all_visible = true, -- Hides everything if all lines are visible
-				throttle_ms = 100,
-				handle = {
-					text = " ",
-					blend = 0, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
-					-- color = "NormalFloat",
-					color_nr = nil, -- cterm
-					-- highlight = nil,
-					highlight = "CursorLine",
-				},
-				marks = {
-					Cursor = {
-						text = "•",
-						priority = 0,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "NormalFloat",
-					},
-					Search = {
-						-- text = { "-", "=" },
-						text = { "@" },
-						priority = 1,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "Typedef",
-					},
-					Error = {
-						-- text = { "-", "=" },
-						text = { "!" },
-						priority = 2,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "Exception",
-					},
-					Warn = {
-						-- text = { "-", "=" },
-						text = { "?" },
-						priority = 3,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "WarningMsg",
-					},
-					Info = {
-						-- text = { "-", "=" },
-						text = { "•" },
-						priority = 4,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "DiagnosticVirtualTextInfo",
-					},
-					Hint = {
-					-- text = { "-", "=" },
-						tet = { "•" },
-						priority = 5,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "Conditional",
-					},
-					Misc = {
-						-- text = { "-", "=" },
-						text = { "•" },
-						priority = 6,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "NormalFloat",
-					},
-					GitAdd = {
-						text = "+",
-						priority = 7,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "GitSignsAdd",
-					},
-					GitChange = {
-						text = "~",
-						priority = 7,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "GitSignsChange",
-					},
-					GitDelete = {
-						text = "x",
-						priority = 7,
-						gui = nil,
-						color = nil,
-						cterm = nil,
-						color_nr = nil, -- cterm
-						highlight = "GitSignsDelete",
-					},
-				},
-				excluded_buftypes = {
-					"terminal",
-				},
-				excluded_filetypes = {
-					"dropbar_menu",
-					"neo-tree",
-					"dropbar_menu_fzf",
-					"DressingInput",
-					"cmp_docs",
-					"cmp_menu",
-					"noice",
-					"prompt",
-					"TelescopePrompt",
-				},
-				autocmd = {
-					render = {
-						"BufWinEnter",
-						"TabEnter",
-						"TermEnter",
-						"WinEnter",
-						"CmdwinLeave",
-						"TextChanged",
-						"VimResized",
-						"WinScrolled",
-					},
-					clear = {
-						"BufWinLeave",
-						"TabLeave",
-						"TermLeave",
-						"WinLeave",
-					},
-				},
-				handlers = {
-					cursor = false,
-					diagnostic = true,
-					gitsigns = false, -- Requires gitsigns
-					handle = true,
-					search = false, -- Requires hlslens
-					ale = false, -- Requires ALE
-				},
-			})
-		end,
-	},
+  {
+    "petertriho/nvim-scrollbar",
+    config = function()
+      require("scrollbar").setup({
+        show = true,
+        show_in_active_only = true,
+        set_highlights = true,
+        folds = true,           -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
+        max_lines = false,      -- disables if no. of lines in buffer exceeds this
+        hide_if_all_visible = true, -- Hides everything if all lines are visible
+        throttle_ms = 100,
+        handle = {
+          text = " ",
+          blend = 0, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+          -- color = "NormalFloat",
+          color_nr = nil, -- cterm
+          -- highlight = nil,
+          -- highlight = "CursorLine",
+          highlight = "PmenuThumb",
+        },
+        marks = {
+          Cursor = {
+            text = "•",
+            priority = 0,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "NormalFloat",
+          },
+          Search = {
+            -- text = { "-", "=" },
+            text = { "@" },
+            priority = 1,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "Typedef",
+          },
+          Error = {
+            -- text = { "-", "=" },
+            text = { "!" },
+            priority = 2,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "Exception",
+          },
+          Warn = {
+            -- text = { "-", "=" },
+            text = { "?" },
+            priority = 3,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "WarningMsg",
+          },
+          Info = {
+            -- text = { "-", "=" },
+            text = { "•" },
+            priority = 4,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "DiagnosticVirtualTextInfo",
+          },
+          Hint = {
+            -- text = { "-", "=" },
+            tet = { "•" },
+            priority = 5,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "Conditional",
+          },
+          Misc = {
+            -- text = { "-", "=" },
+            text = { "•" },
+            priority = 6,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "NormalFloat",
+          },
+          GitAdd = {
+            text = "+",
+            priority = 7,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "GitSignsAdd",
+          },
+          GitChange = {
+            text = "~",
+            priority = 7,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "GitSignsChange",
+          },
+          GitDelete = {
+            text = "x",
+            priority = 7,
+            gui = nil,
+            color = nil,
+            cterm = nil,
+            color_nr = nil, -- cterm
+            highlight = "GitSignsDelete",
+          },
+        },
+        excluded_buftypes = {
+          "terminal",
+        },
+        excluded_filetypes = {
+          "dropbar_menu",
+          "neo-tree",
+          "dropbar_menu_fzf",
+          "DressingInput",
+          "cmp_docs",
+          "cmp_menu",
+          "noice",
+          "prompt",
+          "TelescopePrompt",
+        },
+        autocmd = {
+          render = {
+            "BufWinEnter",
+            "TabEnter",
+            "TermEnter",
+            "WinEnter",
+            "CmdwinLeave",
+            "TextChanged",
+            "VimResized",
+            "WinScrolled",
+          },
+          clear = {
+            "BufWinLeave",
+            "TabLeave",
+            "TermLeave",
+            "WinLeave",
+          },
+        },
+        handlers = {
+          cursor = false,
+          diagnostic = true,
+          gitsigns = false, -- Requires gitsigns
+          handle = true,
+          search = false, -- Requires hlslens
+          ale = false, -- Requires ALE
+        },
+      })
+    end,
+  },
 }

0b06d9c22fa315039f58236b8623fb95fc7a42da
michaelmechenko <michael.mechenko@gmail.com>
2025-10-09 12:07:07 -0400

    add sidekick

---
+++ nvim/lua/plugins/sidekick.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      nes = { enabled = false },
+      cli = {
+        mux = {
+          backend = "tmux",
+          enabled = true,
+        },
+        create = "window", ---@type "terminal"|"window"|"split"
+        split = {
+          vertical = true, -- vertical or horizontal split
+          size = 0.5,      -- size of the split (0-1 for percentage)
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      -- {
+      --   "<tab>",
+      --   function()
+      --     -- if there is a next edit, jump to it, otherwise apply it if any
+      --     if not require("sidekick").nes_jump_or_apply() then
+      --       return "<Tab>" -- fallback to normal tab
+      --     end
+      --   end,
+      --   expr = true,
+      --   desc = "Goto/Apply Next Edit Suggestion",
+      -- },
+      {
+        "<C-z>",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
+      {
+        "<leader>ms",
+        function() require("sidekick.cli").select() end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
+      },
+      {
+        "<leader>mt",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>mf",
+        function() require("sidekick.cli").send({ msg = "{file}" }) end,
+        desc = "Send File",
+      },
+      {
+        "<leader>mv",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>mp",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+    },
+  }
+}

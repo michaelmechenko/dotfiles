01dd4514641f4c5992ae6b6ad2273dc084c1b49a
michaelmechenko <michael.mechenko@gmail.com>
2025-10-16 02:28:24 -0400

    fix ghost text multiline

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -33,26 +33,6 @@ return {
         nerd_font_variant = 'normal' -- default = mono
       },
 
-      menu = {
-        min_width = 30,
-        -- avoid multi-line completion ghost_text
-        direction_priority = function()
-          local ctx = require('blink.cmp').get_context()
-          local item = require('blink.cmp').get_selected_item()
-          if ctx == nil or item == nil then return { 's', 'n' } end
-
-          local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
-          local is_multi_line = item_text:find('\n') ~= nil
-
-          -- after showing the menu upwards, we want to maintain that direction
-          -- until we re-open the menu, so store the context id in a global variable
-          if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
-            vim.g.blink_cmp_upwards_ctx_id = ctx.id
-            return { 'n', 's' }
-          end
-          return { 's', 'n' }
-        end,
-      },
 
       signature = {
         enabled = true,
@@ -72,7 +52,7 @@ return {
         },
         window = {
           border = 'single',
-          scrollbar = false,
+          scrollbar = true,
           treesitter_highlighting = true,
           show_documentation = false,
         },
@@ -80,13 +60,30 @@ return {
 
       completion = {
         menu = {
+          -- avoid multi-line completion ghost_text
+          direction_priority = function()
+            local ctx = require('blink.cmp').get_context()
+            local item = require('blink.cmp').get_selected_item()
+            if ctx == nil or item == nil then return { 's', 'n' } end
+
+            local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+            local is_multi_line = item_text:find('\n') ~= nil
+
+            -- after showing the menu upwards, we want to maintain that direction
+            -- until we re-open the menu, so store the context id in a global variable
+            if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+              vim.g.blink_cmp_upwards_ctx_id = ctx.id
+              return { 'n', 's' }
+            end
+            return { 's', 'n' }
+          end,
           border = 'single',
-          scrollbar = false,
+          scrollbar = true,
         },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 800,
-          window = { border = 'single', scrollbar = false, }
+          window = { border = 'single', scrollbar = true, }
         },
         ghost_text = {
           enabled = true
@@ -94,7 +91,7 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'snippets' },
       },
 
       fuzzy = { implementation = "prefer_rust_with_warning" }

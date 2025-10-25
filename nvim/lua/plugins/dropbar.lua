022cc0aa732daa1fd4b94fe4a25172dcd8c65b58
michaelmechenko <michael.mechenko@gmail.com>
2025-10-25 18:28:01 -0400

    install dropbar

---
+++ nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    opts = {
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+      require('dropbar').setup({
+        sources = {
+          path = {
+            max_depth = 3
+          }
+        }
+      })
+    end,
+  }
+}

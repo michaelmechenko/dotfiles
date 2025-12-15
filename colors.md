35e2f1585134aad3891c238012e90d7c6a1747fb
michaelmechenko <michael.mechenko@gmail.com>
2025-12-14 20:18:46 -0500

    colors

---
+++ colors.md
@@ -0,0 +1,26 @@
+vague colors:
+normal (nvim) - #90a0b5
+regular: light pink
+zoomed: pale lilac
+float: yellow
+ephemeral: red
+
+light/dark:
+pale lilac -  `LIGHT` - #bebedb - `DARK` - #aeaed1
+light pink - `LIGHT` - #c9b1c9 - `DARK` - #bb9dbd
+blue - `LIGHT` - #8ba9c1 - `DARK` - #6f95b2
+yellow - `LIGHT` - #F5CC96 -`DARK` - #F3BE7B
+orange - `LIGHT` - #E8B589
+red - `LIGHT` - #DF8297 - `DARK` - #D9647E 
+green - #99B782 - `DARK` - #80A563
+GRAY - 606079
+WHITE - cdcdcd
+bg=1c1c24
+
+OLDWORLD:
+[PURPLE] - `LIGHT` - bb9af7 - `DARK` - 826bac
+[GREEN] - `LIGHT` - 8fb995 - `DARK` - 6b8a73
+[BLUE] - `LIGHT` - 92a2d5 - `DARK` - 4D6180
+[RED] - `LIGHT` - f5919b -  `DARK` - 936057
+GRAY - 656a80
+WHITE - a9b1d6
--- nvim/lua/plugins/vague.lua
+++ nvim/lua/plugins/vague.lua
@@ -60,7 +60,35 @@ return {
           match = "bold",
         },
       },
-
+      -- -- Override highlights or add new highlights
+      -- on_highlights = function(highlights, colors) end,
+      --
+      -- -- Override colors
+      -- colors = {
+      --   bg = "#141415",
+      --   inactiveBg = "#1c1c24",
+      --   fg = "#cdcdcd",
+      --   floatBorder = "#878787",
+      --   line = "#252530",
+      --   comment = "#606079",
+      --   builtin = "#b4d4cf",
+      --   func = "#c48282",
+      --   string = "#e8b589",
+      --   number = "#e0a363",
+      --   property = "#c3c3d5",
+      --   constant = "#aeaed1",
+      --   parameter = "#bb9dbd",
+      --   visual = "#333738",
+      --   error = "#d8647e",
+      --   warning = "#f3be7c",
+      --   hint = "#7e98e8",
+      --   operator = "#90a0b5",
+      --   keyword = "#6e94b2",
+      --   type = "#9bb4bc",
+      --   search = "#405065",
+      --   plus = "#7fa563",
+      --   delta = "#f3be7c",
+      -- },
     })
     vim.cmd("colorscheme vague")
   end

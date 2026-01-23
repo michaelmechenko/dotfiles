122fd803080698886555f4a635febf96d73a3036
michaelmechenko <michael.mechenko@tulip.co>
2026-01-23 13:02:32 -0500

    toggle numhl

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,8 +3,8 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
-        signcolumn          = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl               = false, -- Toggle with `:Gitsigns toggle_numhl`
+        signcolumn          = false,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl               = true, -- Toggle with `:Gitsigns toggle_numhl`
         linehl              = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff           = false, -- Toggle with `:Gitsigns toggle_word_diff`
         current_line_blame  = false, -- Toggle with `:Gitsigns toggle_current_line_blame`

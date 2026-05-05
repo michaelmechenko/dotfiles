66ca8f1fca3bca49d7d09f8668fcd0ae3852e989
michaelmechenko <michael.mechenko@gmail.com>
2026-05-04 23:05:16 -0400

    disable telescope

---
+++ nvim/lua/disabled-plugins/telescope.lua
@@ -0,0 +1,4 @@
+-- Removed: telescope.nvim and telescope-ui-select.nvim
+-- Snacks picker replaces Telescope with full feature parity
+-- including colorschemes, highlights, and vim.ui.select (enabled by default)
+return {}
